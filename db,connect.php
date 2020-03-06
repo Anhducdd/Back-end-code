cd73fd14829b7fb8dd08367cf1fa63d5fe17440b
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-03-06 11:57:06 +0700

    Create db,connect.php

---
+++ db,connect.php
@@ -0,0 +1,6 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "duchieu";
+$dbPassword = "Duchieu123@";
+$dbName = "ducdb";
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
