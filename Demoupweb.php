a6c774e3cf02d3c6cf36abe0464f3ffc4499aa01
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-16 15:56:54 +0700

    Update Demoupweb.php

--- Demoupweb.php
+++ Demoupweb.php
@@ -1,12 +1,3 @@
 <?php
 echo "Welcome to DUC web";
-<form action="#" method="POST" id="formLogin">
-            <label>User</label><br/>
-            <input name="user" type="text" id="user" /><br/>
-            <label>Password</label><br/>
-            <input name="password" type="password" id="password" /><br/><br/>
-            <input type="submit" name="Submit" value="LOGIN" />
-
-</form>
-
 ?>
